43b22c6e1b407f421f82e357145cdc1aa33c6b87
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-12-28 09:19:18 +0100

    trying out playwright

--- src/app/adapters/stuttgart_booksrepo.py
+++ src/app/adapters/stuttgart_booksrepo.py
@@ -6,6 +6,9 @@ from src.app.app import BooksRepo, CheckedOutBook
 
 class StuttgartLibraryBooksRepo(BooksRepo):
 
+    def __init__(self) -> None:
+        ...
+
     def check_out_book(self, title: str, due_on: datetime, extensions: int):
         raise IOError("Not possible. This method only meant for testing.")
 
